ecf9cb0be4412b9e59f125a98536ab0b1c133382
smonagan <71908863+smonagan@users.noreply.github.com>
2025-04-10 16:25:27 -0700

    comment fix

--- Programming Files/pc_files/main.py
+++ Programming Files/pc_files/main.py
@@ -106,7 +106,6 @@ def main():
     #Incomplete.
     # Run Raspberry Pi Python script via SSH in the background
     # Power cycle raspbery pi in order to reset sockets, or implement code to close sockets when gui closes
-
     threading.Thread(target=run_rpi_python_file, daemon=True).start()
 
     def on_closing():  
